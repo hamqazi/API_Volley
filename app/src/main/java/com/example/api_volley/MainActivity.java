dfe3dceafe7d9d9c8afe0a16dff74bfc215d141e
Hammad <hamqazi93@gmail.com>
2019-06-06 21:44:24 +1200

    Added Request Queue

--- app/src/main/java/com/example/api_volley/MainActivity.java
+++ app/src/main/java/com/example/api_volley/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.api_volley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.text);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url="https://anapioficeandfire.com/api/characters/583";
+
+
     }
 }
